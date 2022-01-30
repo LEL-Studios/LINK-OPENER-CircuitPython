5a7b40552c4e9b8adac1706acbd2ee935909d446
lucaso60 <lucasoAlt@outlook.com>
2022-01-30 15:53:19 +0100

    Added all files and did some copyright stuff

--- LICENSE.md
+++ LICENSE.md
@@ -1,24 +1,10 @@
-# LINK-OPENER-CircuitPython
-A link opener library for circuitpython boards. 
+# LICENSE INFORMATION
 
-# Installation 
-1. Install CircuitPython on your board, use this link: https://circuitpython.org/downloads. Choose your board, for me it was the Raspberry Pi Pico, but it may be different for you.
-2. Install the CircuitPython HID Driver, you can use the CircuitPython Driver in this repository under the `lib` folder or you can download it from: https://circuitpython.org/libraries. If you downloaded the bundle, you have to scroll down till you find a folder name `adafruit_hid` and `hid.mpy`. Then place the files under the lib folder.
-3. Download this repository, then place the `code.py` file in the root of you board.
-4. Go to the `lib` folder and find `golink.py`, place that file in the root of your board.
-5. If you want to make your own `code.py` file you can do this: <br>
-<br>
-```
-from golink import golink
+The following document is License information for the "Software". To use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software you shall follow the below copyright notice.
 
-golink("https://google.com") # Replace the URL with your own.
+___
 
-
-
-...
-```
-
-# LICENSE
 ```
 MIT License
 
@@ -55,7 +41,3 @@ Individual files may include additional copyright holders. Licenses for the othe
         /adafruit_hid (MIT)
         hid.mpy (MIT)
 ```
-
-# Contact
-You can contact us at dev.lelstudios@outlook.com
-
---
+++ lib/golink.py
@@ -0,0 +1,186 @@
+"""
+MIT License
+
+Copyright(c) 2021 lucaso60
+Copyright(c) 2021 LEL Studios
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import time
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard import Keyboard
+import usb_hid
+
+
+def golink(link):
+    print("Setting up keyboard...")
+    kbd = Keyboard(usb_hid.devices)
+    time.sleep(0.5)
+
+    kbd.send(Keycode.WINDOWS, Keycode.R)
+    kbd.release_all()
+
+    time.sleep(1)
+
+    URL = f"microsoft-edge:{link}"
+    for char in URL:
+        print(char)
+        if char == ":":
+            kbd.send(Keycode.SHIFT, Keycode.SEMICOLON)
+        elif char == "/":
+            kbd.send(Keycode.FORWARD_SLASH)
+        elif char == ".":
+            kbd.send(Keycode.PERIOD)
+        elif char == "?":
+            kbd.send(Keycode.SHIFT, Keycode.FORWARD_SLASH)
+        elif char == "=":
+            kbd.send(Keycode.EQUALS)
+        elif char == "-":
+            kbd.send(Keycode.MINUS)
+        else:
+            send(char, kbd)
+        kbd.release_all()
+
+    kbd.release_all()
+
+    kbd.send(Keycode.ENTER)
+    kbd.release_all()
+
+
+def send(keycode, kbd):
+    if keycode == "a":
+        kbd.send(Keycode.A)
+    elif keycode == "b":
+        kbd.send(Keycode.B)
+    elif keycode == "c":
+        kbd.send(Keycode.C)
+    elif keycode == "d":
+        kbd.send(Keycode.D)
+    elif keycode == "e":
+        kbd.send(Keycode.E)
+    elif keycode == "f":
+        kbd.send(Keycode.F)
+    elif keycode == "g":
+        kbd.send(Keycode.G)
+    elif keycode == "h":
+        kbd.send(Keycode.H)
+    elif keycode == "i":
+        kbd.send(Keycode.I)
+    elif keycode == "j":
+        kbd.send(Keycode.J)
+    elif keycode == "k":
+        kbd.send(Keycode.K)
+    elif keycode == "l":
+        kbd.send(Keycode.L)
+    elif keycode == "m":
+        kbd.send(Keycode.M)
+    elif keycode == "o":
+        kbd.send(Keycode.O)
+    elif keycode == "p":
+        kbd.send(Keycode.P)
+    elif keycode == "q":
+        kbd.send(Keycode.Q)
+    elif keycode == "r":
+        kbd.send(Keycode.R)
+    elif keycode == "s":
+        kbd.send(Keycode.S)
+    elif keycode == "t":
+        kbd.send(Keycode.T)
+    elif keycode == "u":
+        kbd.send(Keycode.U)
+    elif keycode == "v":
+        kbd.send(Keycode.V)
+    elif keycode == "w":
+        kbd.send(Keycode.W)
+    elif keycode == "x":
+        kbd.send(Keycode.X)
+    elif keycode == "y":
+        kbd.send(Keycode.Y)
+    elif keycode == "z":
+        kbd.send(Keycode.Z)
+    elif keycode == "1":
+        kbd.send(Keycode.ONE)
+    elif keycode == "2":
+        kbd.send(Keycode.TWO)
+    elif keycode == "3":
+        kbd.send(Keycode.THREE)
+    elif keycode == "4":
+        kbd.send(Keycode.FOUR)
+    elif keycode == "5":
+        kbd.send(Keycode.FIVE)
+    elif keycode == "6":
+        kbd.send(Keycode.SIX)
+    elif keycode == "7":
+        kbd.send(Keycode.SEVEN)
+    elif keycode == "8":
+        kbd.send(Keycode.EIGHT)
+    elif keycode == "9":
+        kbd.send(Keycode.NINE)
+
+    if keycode == "A":
+        kbd.send(Keycode.SHIFT, Keycode.A)
+    elif keycode == "B":
+        kbd.send(Keycode.SHIFT, Keycode.B)
+    elif keycode == "C":
+        kbd.send(Keycode.SHIFT, Keycode.C)
+    elif keycode == "D":
+        kbd.send(Keycode.SHIFT, Keycode.D)
+    elif keycode == "E":
+        kbd.send(Keycode.SHIFT, Keycode.E)
+    elif keycode == "F":
+        kbd.send(Keycode.SHIFT, Keycode.F)
+    elif keycode == "G":
+        kbd.send(Keycode.SHIFT, Keycode.G)
+    elif keycode == "H":
+        kbd.send(Keycode.SHIFT, Keycode.H)
+    elif keycode == "I":
+        kbd.send(Keycode.SHIFT, Keycode.I)
+    elif keycode == "J":
+        kbd.send(Keycode.SHIFT, Keycode.J)
+    elif keycode == "K":
+        kbd.send(Keycode.SHIFT, Keycode.K)
+    elif keycode == "L":
+        kbd.send(Keycode.SHIFT, Keycode.L)
+    elif keycode == "M":
+        kbd.send(Keycode.SHIFT, Keycode.M)
+    elif keycode == "O":
+        kbd.send(Keycode.SHIFT, Keycode.O)
+    elif keycode == "P":
+        kbd.send(Keycode.SHIFT, Keycode.P)
+    elif keycode == "Q":
+        kbd.send(Keycode.SHIFT, Keycode.Q)
+    elif keycode == "R":
+        kbd.send(Keycode.SHIFT, Keycode.R)
+    elif keycode == "S":
+        kbd.send(Keycode.SHIFT, Keycode.S)
+    elif keycode == "T":
+        kbd.send(Keycode.SHIFT, Keycode.T)
+    elif keycode == "U":
+        kbd.send(Keycode.SHIFT, Keycode.U)
+    elif keycode == "V":
+        kbd.send(Keycode.SHIFT, Keycode.V)
+    elif keycode == "W":
+        kbd.send(Keycode.SHIFT, Keycode.W)
+    elif keycode == "X":
+        kbd.send(Keycode.SHIFT, Keycode.X)
+    elif keycode == "Y":
+        kbd.send(Keycode.SHIFT, Keycode.Y)
+    elif keycode == "Z":
+        kbd.send(Keycode.SHIFT, Keycode.Z)
